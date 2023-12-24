67c5df9802c879a6db7eb3511a4ad46d2895fa2e
Matheus <matheustorresdev@gmail.com>
2023-12-24 10:52:30 -0300

    Finalizando Modal de Nova Transação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
+import { NewTransactionModal } from "../NewTransactionModal";
+
 import logoImg from "../../assets/logo.svg";
 
 export const Header = () => {
@@ -14,15 +16,7 @@ export const Header = () => {
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
